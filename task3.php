7822d5ebd1ff9f58f59bf9c549953537bb59fc6d
Stanislav B <stas@biche-ool.ru>
2021-07-04 16:46:08 +0700

    Task 3 v2

--- task3.php
+++ task3.php
@@ -6,10 +6,10 @@ print_r(minAbsDiff($arr));
 function minAbsDiff(array $array): int
 {
     $minAbsDiff = INF;
-    foreach ($array as $iKey => $iValue) {
-        foreach ($array as $jKey => $jValue) {
-            if ($iKey === $jKey) { continue; }
-            $absDiff = abs($iValue - $jValue);
+    $n = count($array);
+    for ($i = 0; $i < $n - 1; $i++) {
+        for ($j = $i + 1; $j < $n; $j++) {
+            $absDiff = abs($array[$i] - $array[$j]);
             if ($absDiff < $minAbsDiff) {
                 $minAbsDiff = $absDiff;
             }
