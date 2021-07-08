f58cfa894a355f990151a4c8f75091553cbfa86f
Stanislav B <stas@biche-ool.ru>
2021-07-08 17:13:19 +0700

    Fix #2

--- src/task4.php
+++ src/task4.php
@@ -5,7 +5,9 @@ function pairs(array $arr, int $k): int
     $result = 0;
     foreach ($arr as $iKey => $iValue) {
         foreach ($arr as $jKey => $jValue) {
-            if ($iKey === $jKey) { continue; }
+            if ($iKey === $jKey) {
+                continue;
+            }
             if ($iValue - $jValue === $k) {
                 $result++;
             }
